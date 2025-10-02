96103bade024d8441bc0675ad6a3923958e5f192
NotJustCatCoder <1357329777@qq.com>
2025-10-02 13:33:37 +0800

    创建初始项目

---
+++ src/main/java/com/springai/springaivideoextension/SpringAiVideoExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.springai.springaivideoextension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiVideoExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiVideoExtensionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springai/springaivideoextension/SpringAiVideoExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.springaivideoextension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiVideoExtensionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
